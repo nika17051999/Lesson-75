06a1a38a792a3407c8c5df777bd54411ba8c05ba
nika sakhuria <nikasakhuria17051999@gmail.com>
2023-03-27 09:57:21 +0400

    lesson 75

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+delegate void NotificationDelegate(string text);
+delegate int OperationDelegate(int X, int Y);
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        OperationDelegate operationDelegate = delegate (int firstParameter, int secondParameter) 
+        { return firstParameter + secondParameter; } ;
+
+        int result = operationDelegate(1, 5);
+
+        Console.WriteLine(result);
+
+
+    }
+
+   
+    //private static void AddInCOnsole( string text)
+    //{
+    //    Console.WriteLine(text);
+    //}
+    //    private static void AddInFile(string text)
+    //    {
+    //        string path = @"C:\Users\Grinch\Desktop\New folder\Logger.txt";
+    //        File.WriteAllText(path, text);
+    //    }
+}
